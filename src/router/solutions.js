92af3612300135db9f7cb5a7fdd746aefcab61d7
Shekha-Maha <shekhamaha9@gmail.com>
2024-06-27 01:49:06 +0600

    about section done contact section on processing

--- src/router/solutions.js
+++ src/router/solutions.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 router.get('/', (req, res) => {
-    res.render('layout', { page: 'solutions', title: 'Solutions', heroTitle: 'We provide services', heroSubtitle: 'Pelyform harnesses technology to create innovative solutions. We empower businesses to thrive in a digital world. Discover how our products can transform your operations.', buttonTitle:'Learn more', heroImage: 'home-hero.png'});
+    res.render('layout', { page: 'solutions', title: 'Solutions', heroTitle: 'Unleashing Innovation with Pelyform', heroSubtitle: 'Pelyform harnesses technology to create innovative solutions. We empower businesses to thrive in a digital world. Discover how our products can transform your operations.', buttonTitle:'Learn more', heroImage: 'home-hero.png'});
 });
 
 
